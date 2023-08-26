ca190072e846bc39d9c1bec05eec71d2296e01f9
Emilio Morles <emiliomorles>
2023-08-26 16:20:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# BitQuest: The Bitcoin Treasure Hunt
+
+Developer: @Emilio_Morles
+
+Embark on an exhilarating quest in "BitQuest: The Bitcoin Treasure Hunt"! Choose your path wisely as you navigate through the Bitcoin jungle, overcome obstacles, and face thrilling challenges. Find the ultimate Bitcoin treasure by making strategic decisions, solving puzzles, and exploring mysterious caves. Will you claim the prize or face the consequences? Dive into this captivating adventure and discover the excitement of bitcoin a fictional world of adventure. Remember, the treasure is waiting for you, but only the cleverest and bravest will prevail!
---
+++ main.py
@@ -0,0 +1,55 @@
+print(r'''
+                ⣀⣤⣴⣶⣾⣿⣿⣿⣿⣷⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀
+        ⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+        ⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀
+        ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣤⣄⠀⠀⠀⠈⠉⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+        ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢠⣶⣶⣤⡀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡆
+        ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠼⣿⣿⡿⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢀⣀⣀⠀⠀⠀⠀⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        ⢿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠀⠀⣼⣿⣿⣿⣦⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡿
+        ⠸⣿⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠛⠛⠿⠟⠋⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⠇
+        ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣤⡄⠀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⡟⠀
+        ⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀
+        ⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀
+                            _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \'.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\\/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+          bitcoin '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+''')
+
+print("Welcome to Bitcoin Treasure Jungle.")
+print("Your mission is to find the bitcoin treasure.") 
+#diagram = https://app.diagrams.net/?src=about#Hemiliomorles%2Fdiagrams.net%2Fmain%2Fbitcoin_game_diagram.drawio
+choice1 = input('\nYou are in the bitcoin jungle now. Your first adventure bring you to two caves. \nTo your "Left" you have the Bitcoin Core cave and to your "Right" you have the Bitcoin Cash cave. Which one would you enter?\n').lower()
+if choice1 == "left":
+  # CONTINUE
+  choice2 = input('\nYou chose wisely. Now you need to decide between "swim" under the cave following the Satoshi path or "jump" over the Vitalik wall?\n').lower()
+  if choice2 == "swim":
+    choice3 = input('\nAmazing!. You are now in the final quest. \nThere are four doors in front of you, each one with a different color, "White", "Black", "Orange" and "Black". \nWhich door are you going to open?\n').lower()
+    if choice3 == "orange":
+      print("\nCongratulation!, You win the bitcoin treasure.\n" + "Now go to bitcoin.org so you can find your way to a whole bitcoin.")
+    else:
+      print("\nSorry, you are dead. You needed to open the Orange door")  
+  else:
+    print("\nSorry, you are dead. Vitalik is a sh*tcoin creator")
+  pass
+else:
+  print("\nSorry, you are dead. Bitcoin Cash is a Scam because is centralized")
